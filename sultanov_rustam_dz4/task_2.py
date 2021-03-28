e48375d2c119ab02902be7c06f9fdb273fa1739e
Rustam <spycer@yandex.ru>
2021-03-28 23:37:10 +0300

    Merge pull request #1 from Spyker086/task_1,2
    Задание 1 и 2

---
+++ sultanov_rustam_dz4/task_2.py
@@ -0,0 +1,29 @@
+import xml.etree.ElementTree as ET
+import urllib.request
+from decimal import Decimal
+
+
+def currency_rates(currency):
+    tree = ET.parse(urllib.request.urlopen('http://www.cbr.ru/scripts/XML_daily.asp'))
+    root = tree.getroot()
+
+    list_charcode = []
+    list_value = []
+
+    for elem in root:
+        list_charcode.append(elem[1].text.lower())
+
+    for elem in root:
+        list_value.append(elem[4].text)
+
+    dict_total = dict(zip(list_charcode, list_value))
+    p = dict_total.setdefault(currency, '0')
+    p = p.replace(',','.')
+    p = Decimal(p)
+    if p != 0:
+        print(f'Текущий курс: {p.quantize(Decimal("1.0000"))} руб')
+    else:
+        print('!!!Нет такой валюты!!!')
+
+currency = input('Ведите валюту: ')
+currency_rates(currency.lower())
