d2c3ddaa9b3c3834a8fb09f11f611a01c5203830
Rustam <spycer@yandex.ru>
2021-04-11 22:17:18 +0300

    Merge pull request #1 from Spyker086/task_1,2
    Задание 1 и 2

---
+++ sultanov_rustam_dz8/task_1.py
@@ -0,0 +1,17 @@
+import re
+
+
+def email_parse(email):
+    keys = ['username', 'domain']
+    # try:
+    pattern = r'\w+[@]\w+[.]\w+'
+    email_re = re.compile(pattern)
+    if email_re.findall(email):
+        pattern2 = r'@'
+        dict_email = dict(zip(keys ,re.split(pattern2, email)))
+        return print(dict_email)
+    else:
+        print("Error: invalid email")
+
+
+email_parse('spycer@yandex.ru')
